cc9e260603f798c5ecd67b5cee0b6107f37c4bea
Cristian Gonzalez <82175293+n0gu1@users.noreply.github.com>
2025-08-01 01:14:16 -0600

    Modelo Producto con asignación masiva configurada

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'precio',
+        'imagen',
+    ];
+
 }
