5663610ab027fcbf80bcb11337b84044b80f34fa
Cristian Gonzalez <82175293+n0gu1@users.noreply.github.com>
2025-07-31 23:54:36 -0600

    Rutas productos CRUD + PDF export

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\ProductoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('productos', ProductoController::class);
+
+Route::get('productos-pdf', [ProductoController::class, 'exportPdf'])
+     ->name('productos.pdf');
